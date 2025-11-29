d855d3adad9de271ecdf0a829335b9f0407cbdb0
Jahanpoor <test.developer.test.all@gmail.com>
2025-11-29 13:40:13 +0330

    add form validate

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -37,11 +37,29 @@ public class WeatherForecastController : ControllerBase
     {
         return Ok(model);
     }
+
+    [HttpPost("Form")]
+    public IActionResult PostForm([FromForm] TestModel2 model)
+    {
+        return Ok(model);
+    }
 }
 
 public class TestModel
 {
     public string? Name { get; set; }
+}
+
+public class TestModel2
+{
+    public int Id { get; set; }
+
+    public IFormFile[] Files { get; set; } = [];
+
+    public IFormFile? File
+    {
+        get; set;
+    }
 }
 
 //3
@@ -53,6 +71,25 @@ public class TestModelValidator : IAsyncValidation<TestModel>
 
         if (string.IsNullOrWhiteSpace(model.Name))
             val.AddField(nameof(model.Name), $"{model.Name}", "Name is required!");
+
+        return val;
+    }
+}
+
+public class TestModel2Validator : IAsyncValidation<TestModel2>
+{
+    public async Task<Validation> ValidateAsync(TestModel2 model)
+    {
+        var val = Validation.Create();
+
+        if (model.Id <= 0)
+            val.AddField(nameof(model.Id), $"{model.Id}", "Id is not valid!");
+
+        if (model.Files == null || !model.Files.Any())
+            val.AddField(nameof(model.Files), $"", "Files is required!");
+
+        if (model.File == null)
+            val.AddField(nameof(model.File), $"", "File is required!");
 
         return val;
     }
